659a9862f4ea709bde65c6ac72dffe3248cb44f2
Mazen Amir <49759150+Selvster@users.noreply.github.com>
2025-04-23 07:09:38 +0300

    static add env

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -20,7 +20,7 @@ export const scalars = ZeusScalars({
 
 //use 'http://localhost:3000/shop-api/' in local .env file for localhost development and provide env to use on prod/dev envs
 
-export const VENDURE_HOST = `${process.env.NEXT_PUBLIC_HOST ?? 'https://changing-elfie-mahmoudsadeq-c3759bd2.koyeb.app'}/shop-api`;
+export const VENDURE_HOST = `https://changing-elfie-mahmoudsadeq-c3759bd2.koyeb.app/shop-api`;
 
 const apiFetchVendure =
     (options: fetchOptions) =>
